a5de2f1f38c4415117a40366adfd6f2496d9eceb
K@ushlendra Pratap Singh <118584505+singhskaushal49@users.noreply.github.com>
2024-03-07 14:12:59 +0600

    Message.py
    Add Message.py

---
+++ Message.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+time.sleep(4)
+for i in range(20):
+    pyautogui.typewrite("Kaushal Singh")
+    pyautogui.press("enter")
+    pyautogui.typewrite("Deeplai Singh")
+    pyautogui.press("enter")
+    pyautogui.typewrite("lapu sa sachin")
+    pyautogui.press("enter")
